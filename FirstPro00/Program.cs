88407473bdcaacecf0e6f33d99cddc9297a3af81
nikitaOsipof <nikita@ifmo.spb.ru>
2024-04-26 21:45:19 +0300

    Add y1

---
+++ FirstPro00/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstPro00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = 2.7, x2 = 5.8;
+            double y1 = x1 + x2;
+            Console.WriteLine($"Hello, {y1}");
+        }
+    }
+}
