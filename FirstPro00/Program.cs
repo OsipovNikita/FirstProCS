4c0e302104836e046a54972915356ed25e5e1cfb
nikitaOsipof <nikita@ifmo.spb.ru>
2024-04-27 20:58:49 +0300

    Добавление метода перевода числа в другую систему счисления

--- FirstPro00/Program.cs
+++ FirstPro00/Program.cs
@@ -4,9 +4,11 @@
     {
         static void Main(string[] args)
         {
-            double x1 = 2.7, x2 = 5.8;
+            double x1 = 2.7, x2 = 15.8;
             double y1 = x1 + x2;
             Console.WriteLine($"Hello, {y1}");
+            Console.WriteLine(NumberTranslation.TransNumber((int)y1, 2));
+
         }
     }
 }
