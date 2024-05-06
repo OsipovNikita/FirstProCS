282a67ad1f90a0a60a486f5db21259113e253408
nikitaOsipof <nikita@ifmo.spb.ru>
2024-05-06 20:09:14 +0300

    Проверка даты

--- FirstPro00/Program.cs
+++ FirstPro00/Program.cs
@@ -8,16 +8,15 @@
             double y1 = x1 + x2;
             Console.WriteLine($"Hello, {y1}");
 
+            // Проверка даты и времени
+            Console.WriteLine(DateTimeDemo.DateTimeNow());
+
             // проверка стека
             Console.WriteLine(NumberTranslation.TransNumber((int)y1, 2));
 
             // проверка очереди
             int[] ms = {11,3,41,52,63,71,89,10};
             ReorderingQueue.Reordering(ms);
-
-            // Проверка даты и времени
-            Console.WriteLine(DateTimeDemo.DateTimeNow());
-
         }
     }
 }
