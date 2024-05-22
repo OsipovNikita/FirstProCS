522c723a1708d47293c566095055c7fb7799577d
nikitaOsipof <nikita@ifmo.spb.ru>
2024-05-22 17:54:23 +0300

    Передача параметров в метод

---
+++ FirstPro00/PassParamMetod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPro00
+{
+    struct MyStruct
+    {
+        public int x; 
+        public int y;
+    }
+    internal class PassParamMetod
+    {
+        static void ArgExample(MyStruct number)
+        {
+            number.x = 191;
+        }
+        static void refArgExample(ref MyStruct number)
+        {
+            number.x = 191;
+        }
+        static void refreadonlyArgExample(ref readonly MyStruct number)
+        {
+         //  number.x = 191;
+        }
+        static void inArgExample(in MyStruct number)
+        {
+           // number.x = 191;
+        }
+
+    }
+}
